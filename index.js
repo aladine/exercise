68d6196006452868197047dd473f92c913ddb6d0
Dan <dan.aladine@gmail.com>
2014-02-22 10:24:55 +0800

    temporary commit due to api limit per hour

--- index.js
+++ index.js
@@ -3,13 +3,18 @@
 // Sample for handling the rendering
 
 var emitter = new EventEmitter();
+
+var normalize = function(str){return str.replace('_','.');};
 var api_url = 'https://api.github.com/';
 function loadRepos(user){
 	 emitter.on('data.repos', renderRepos);
 	$.get(api_url+'users/'+user+'/repos',function(repos){
-		console.log(repos);
-		  emitter.emit('data.repos', repos);
-		  repos.forEach(function(repo){
+		if(typeof repos.data.forEach ==='undefined'){
+			alert(!!repos.data.message ? repos.data.message : '?' );
+			return false;
+		}
+		  emitter.emit('data.repos', repos.data);
+		  repos.data.forEach(function(repo){
 		    loadBranches(repo);
 		    loadCollaborators(repo);
 		  });
@@ -19,30 +24,59 @@ function loadRepos(user){
 }
 
 function loadBranches(repo){
-	$.get(repo.html_url+'/branches',function(branches){
-		 emitter.emit('data.branches.'+repo.name, branches);
+	$.get(repo.url+'/branches',function(branches){
+		 emitter.emit('data.branches.'+repo.name, branches.data);
 	},'jsonp');	
 }
 
 function loadCollaborators(repo){
-	$.get(repo.html_url+'/collaborators',function(collaborators){
-		 emitter.emit('data.collaborators.'+repo.name, collaborators);
+	$.get(repo.url+'/collaborators',function(collaborators){
+		 emitter.emit('data.collaborators.'+repo.name, collaborators.data);
 	},'jsonp');	
 }
 
 // render repositories data
-function renderRepos(data, type){ 
+function renderRepos(type,data){ 
+	var list =[];
 	data.forEach(function(repo){
+		
 		    list.push('<tr><td>'
-		    	+'<a href="">'+repo.name+'</a>'
-		    	+)
+		    	+'<a href="#">'+repo.name+'</a>'
+		    	+'<span id="data_branches_'+repo.name+'"></span>'
+		    	+'</td><td id="data_collaborators_'+repo.name+'">'
+		    	+'</td><td>'+repo.created
+		    	+'</td></tr>');
 		  });
-	$('#repos').html()
+	console.log($('tbody','#'+normalize(type)),list);
+	$('tbody','#'+normalize(type)).html(list.join());
 }
 
 // render branches data
-function renderBranches(data, type){ }
+function renderBranches(type,data){ 
+	console.log('renderBranches',type,data);
+	var list =[];
+	data.forEach(function(branch){
+		list.push(branch.name);
+	});
+	$('#'+normalize(type)).html('('+list.join(',')+')');
+}
 
 // render collaborators data
-function renderCollaborators(data, type){ }
+function renderCollaborators(type,data){ 
+	console.log('renderCollaborators',type,data);
+	
+	var list =[];
+	data.forEach(function(collaborator){
+		list.push('<a href="#">'+collaborator.name+'</a>');
+	});
+	$('#'+normalize(type)).html(list.join(','));
+}
+
+$(document).ready(function(){
+	$('input[type="submit"]').on('click',function(e){
+		e.preventDefault();
+		var user = $('input#github').val();
+		loadRepos(user);
+	})
+});
 
